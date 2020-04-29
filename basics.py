917c1a5634a905a9e02813c53251a6a0cb2bec6a
Lokesh Banerjee <lokeshbanerjee@yahoo.co.in>
2020-04-29 23:16:19 +0100

    My first python files

---
+++ basics.py
@@ -0,0 +1,12 @@
+character_name="Loki"
+character_age="34"
+phrase="Max Muller was the captain of the team"
+print("..Hello World..")
+print("I know a guy called "+character_name+ ".")
+print("He is " +character_age+ " years old.")
+print(""+character_name+ " is very hard working.")
+print(phrase)
+print(phrase.lower())
+print(phrase.upper())
+
+
