0bc379159d06c509c8a2333adb0a99b874fca9b0
Lokesh Banerjee <lokeshbanerjee@yahoo.co.in>
2020-04-29 23:15:12 +0100

    Python basics

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,1 +1,9 @@
-print("hello lokesh")
+print("...My First Basic Calculator..")
+my_num=
+print("Enter the First value:")
+print("Enter the second value:")
+print("The answer to your question is ?? = ")
+print("")
+print("")
+print("")
+print("")
