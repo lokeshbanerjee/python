facd1ebdf39612c20973f6113f0016e97b2c0555
Lokesh Banerjee <lokeshbanerjee@yahoo.co.in>
2020-04-29 23:15:36 +0100

    Save current state before upgrade

--- if statement.py
+++ if statement.py
@@ -1,14 +1,17 @@
 print("we will learn the if conditions in four possible combinations.")
-print("1+2= true", "1+2= false", "1 true + 2 false", "1 false + 2 true")
+print("1 true + 2 true = true")
+print("1 false + 2 false = false")
+print("1 true + 2 false")
+print("1 false + 2 true")
 print("")
-print("odering food for my friend")
+print("Odering food for my friend")
 print("==============================================================")
 is_vegetarian = False
 is_nonveg = True
 if is_vegetarian and is_nonveg:
     print("Can I order vege burger and fish pie for my friend.")
 elif is_vegetarian and not is_nonveg:
-    print("Can I order vege burger FOR MY friend.")
+    print("Can I order vege burger For my friend.")
 elif not is_vegetarian and is_nonveg:
     print("Can I order fish pie for my friend.")
 else:
