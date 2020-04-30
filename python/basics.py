b8dbf50000a05a32e725338b22cb03d69b98d436
lokeshbanerjee <64262745+lokeshbanerjee@users.noreply.github.com>
2020-04-30 19:16:53 +0100

    Add files via upload

---
+++ python/basics.py
@@ -0,0 +1,17 @@
+character_name = "Loki"
+character_age = "34"
+phrase = "Max Muller was the captain of the team"
+print("..Hello World..")
+print("I know a guy called "+character_name+".")
+print("He is "+character_age+" years old.")
+print(""+character_name+" is very hard working.")
+print(phrase)
+phrase = phrase.lower()
+print(phrase)
+print(phrase.upper().isupper())
+print(phrase.islower())
+print(len(phrase))
+print(phrase[4])
+print(phrase.index("u"))
+print(phrase.replace("captain", "coach"))
+
---
+++ python/def funtion.py
@@ -0,0 +1,19 @@
+def say_hi():
+    print("hello user")
+print("top")
+say_hi()
+print("bottom")
+print("==================================================")
+
+def say_hello(name):
+    print("Hello " + name)
+
+say_hello("Mike")
+say_hello("steve")
+print("==================================================")
+
+def say(name, age):
+    print("hello "+name+" you are " +age)
+say("mike", "35")
+say("steve", "40")
+print("==================================================")
